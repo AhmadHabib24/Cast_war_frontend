bae68cf96cffb2290b52308054c845e0f8841457
Habib <developerwebsite04@gmail.com>
2026-09-03 19:21:17 +0500

    Revert "Add temporary maintenance mode mask"
    This reverts commit 581ea8e122bcf443f0e0f0ba49c56bfe165b0d33.

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,42 +33,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // Temporary maintenance mode flag
-  const isMaintenance = true;
-
-  if (isMaintenance) {
-    return (
-      <html lang="en" className={`${poppins.variable} h-full antialiased`}>
-        <body className="min-h-full flex flex-col font-sans">
-          <div className="min-h-screen bg-white font-[Arvo,serif] flex items-center justify-center p-4">
-            <style dangerouslySetInnerHTML={{
-              __html: `
-              @import url('https://fonts.googleapis.com/css?family=Arvo');
-              
-              .maintenance_bg {
-                background-image: url(https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif);
-                height: 400px;
-                background-position: center;
-                background-repeat: no-repeat;
-              }
-              `
-            }} />
-
-            <section className="maintenance_page w-full max-w-4xl mx-auto text-center">
-              <div className="maintenance_bg flex justify-center items-start pt-10 rounded-3xl mb-[-50px]">
-              </div>
-
-              <div className="contant_box_404 relative z-10 bg-white inline-block px-10 py-8 rounded-3xl shadow-[0_-20px_40px_rgba(255,255,255,1)]">
-                <h3 className="text-3xl md:text-5xl font-black mb-4">Website is currently Down</h3>
-                <p className="text-xl text-gray-500 mb-8">We are working on it. Please be patient.</p>
-              </div>
-            </section>
-          </div>
-        </body>
-      </html>
-    );
-  }
-
   return (
     <html
       lang="en"
